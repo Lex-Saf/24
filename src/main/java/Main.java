b59fa9830d5209677468985a43f3ee46b4fb290f
Lex-Saf <alex-mezh23@yandex.ru>
2026-04-14 21:19:42 +0300

    Dz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,63 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        int speed;
+
+        System.out.println("24 часа Ле-Мана");
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("\n- Введите название машины №" + i);
+            String name = scanner.next();
+
+            while (true) {
+                System.out.println("\n- Введите скорость машины (0-250 км/ч): ");
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                    if (speed >= 0 && speed <= 250) {
+                        break;
+                    }
+                } else {
+                    scanner.next();
+                }
+                System.out.println("- Неправильная скорость");
+            }
+
+            Car car = new Car(name, speed);
+            race.leader(car);
+        }
+
+        System.out.println("- Самая быстрая машина: " + race.winner());
+
+        scanner.close();
+    }
+
+}
+class Car {
+    String name;
+    int speed;
+
+    Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
     }
 }
+
+class Race {
+    String leaderName = "";
+    int maxDistance = 0;
+
+    public void leader(Car car) {
+        int distance = 24 * car.speed;
+        if (distance > maxDistance) {
+            maxDistance = distance;
+            leaderName = car.name;
+        }
+    }
+
+    public String winner() {
+        return leaderName;
+    }
+}
+
